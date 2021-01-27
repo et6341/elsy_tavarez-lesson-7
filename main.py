e036279552bffd1b1496f4d89a2fee7a86bb59c6
et6341 <72764888+et6341@users.noreply.github.com>
2021-01-27 11:05:20 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 while True:
-    if input.rotation(Rotation.ROLL) < 45:
+    print(input.rotation(Rotation.PITCH))
+    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0:
         light.set_all(light.rgb(255, 0, 255))
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 while (true) {
-    if (input.rotation(Rotation.Roll) < 45) {
+    console.log(input.rotation(Rotation.Pitch))
+    if (input.rotation(Rotation.Pitch) > 0 || input.rotation(Rotation.Pitch) < 0) {
         light.setAll(light.rgb(255, 0, 255))
+    } else {
+        light.clear()
     }
     
 }
